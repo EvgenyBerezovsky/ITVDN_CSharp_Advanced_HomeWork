74fae2aa8b9fbbf0702b206f08385ec188336bb8
Evgen <mousetell@ukr.net>
2024-05-03 23:21:39 +0300

    Task003, Task004 were added

---
+++ Lesson002/Task003/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+
+//  Несколькими способами создайте коллекцию, в которой можно хранить только целочисленные и
+//  вещественные значения, по типу «счет предприятия – доступная сумма» соответственно.
+namespace Task003
+{
+    internal class Program
+    {
+        public static Dictionary<int, double> GetDictionary(int count)
+        {
+            Dictionary<int, double> result = new Dictionary<int, double>();
+            while (result.Count < count) 
+            {
+                int someInt = new Random().Next(0, 100);
+                double someDouble = new Random().Next(1, 100) / 3d ;
+                if (!result.ContainsKey(someInt))
+                result.Add(someInt, someDouble);
+            }
+            return result;
+        }
+        public static SortedDictionary<int, double> GetSortedDictionary(int count)
+        {
+            SortedDictionary<int, double> result = new SortedDictionary<int, double>();
+            while (result.Count < count)
+            {
+                int someInt = new Random().Next(0, 100);
+                double someDouble = new Random().Next(1, 100) / 3d;
+                if (!result.ContainsKey(someInt))
+                    result.Add(someInt, someDouble);
+            }
+            return result;
+        }
+        public static void PrintCollection(IDictionary<int, double> collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.WriteLine(@" {0} - {1:f4}", item.Key, item.Value);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Dictionary<int, double> dictionary = GetDictionary(10);
+            SortedDictionary<int, double> sortedDictionary = GetSortedDictionary(20);
+
+            PrintCollection(dictionary);
+            Console.WriteLine(new string('*', 15));
+            PrintCollection(sortedDictionary);
+        }
+    }
+}
---
+++ Lesson002/Task004/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Runtime.Serialization;
+
+namespace Task004
+{
+    public class MyClass
+    {
+        public int IntProp { get; set; }
+        public double DoubleProp { get; set; }
+    }
+
+    public class MyClassComparer : IEqualityComparer
+    {
+        public new bool Equals(object x, object y)
+        {
+            MyClass item1 = x as MyClass;
+            MyClass item2 = y as MyClass;
+            return item1.IntProp == item2.IntProp;
+        }
+
+        public int GetHashCode(object obj)
+        {
+            return (obj as MyClass).IntProp;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderedDictionary myDictionary = new OrderedDictionary(new MyClassComparer());
+            try
+            {
+                myDictionary.Add(new MyClass() { IntProp = 1, DoubleProp = 1.11 }, "Item1");
+                myDictionary.Add(new MyClass() { IntProp = 2, DoubleProp = 2.11 }, "Item2");
+                myDictionary.Add(new MyClass() { IntProp = 3, DoubleProp = 3.11 }, "Item3");
+                myDictionary.Add(new MyClass() { IntProp = 4, DoubleProp = 4.11 }, "Item4");
+                myDictionary.Add(new MyClass() { IntProp = 3, DoubleProp = 5.11 }, "Item5");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            foreach (DictionaryEntry item in myDictionary)
+            {
+                Console.WriteLine(item.Value);
+            }
+        }
+    }
+}
