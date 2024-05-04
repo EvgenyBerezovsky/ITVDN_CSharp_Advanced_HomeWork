23c163059a34d4576b8b288ee468aa20afe43d15
Evgen <mousetell@ukr.net>
2024-05-04 18:16:02 +0300

    Add Task001 Lesson003

---
+++ Lesson003/Task001/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+// Создайте на диске 100 директорий с именами от Folder_0 до Folder_99, затем удалите их.
+namespace Task001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var directoriInfo =
+            new DirectoryInfo(@"D:\ITVDN\ITVDN_C#_Advanced_HomeWork\Lesson003\Task001");
+            for (int i = 0; i < 100; i++)
+            {
+                directoriInfo.CreateSubdirectory($"Folder_{i}");
+            }
+            
+            Console.ReadKey();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Directory.Delete(String.Format(@"D:\ITVDN\ITVDN_C#_Advanced_HomeWork\Lesson003\Task001\Folder_{0}", i));
+            }
+        }
+    }
+}
